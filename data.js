1e7642d33ad407a06202e19661b308af0f4eaba9
shahzad raza <122221090+shahzadgith@users.noreply.github.com>
2025-03-08 08:30:38 +0000

    datatype

---
+++ data.js
@@ -0,0 +1,32 @@
+// // // "use strict"; it is used for putting the data as new version 
+// // // alert(3+3)
+// // // console.log(3+3); /* we should fellow the code realibility, not use the bad practise/*
+// // console.log("razadeveloper987@gmail.com");
+// // // name of data type 
+// // number
+// // bigint
+// // boolean
+// // string//
+// // // // null=> standalone valuee and undefine...
+// // // let shahzad;(it is known as undefine)
+// // console.log(null == undefined);result is true  
+// // let get =1/0;
+// // console.log(typeof(get));
+// const sym1 = Symbol("description");
+// console.log(sym1);
+// const sym1 = Symbol("description");
+// const sym2 = Symbol("description");
+// console.log(sym1==sym2);
+// Every Symbol value is unique, even if they have the same description.
+// let b = 33;
+// console.log(b);
+// console.log(typeof(b));
+console.log(typeof(null));
+console.log(typeof(Object));
+console.log(typeof(undefined));
+
+
+
+
+
+
---
+++ let.js
@@ -0,0 +1,32 @@
+// // const number= 12345;
+// // var email = "razadeveloper233@gmail.com"
+// // let Emailaddress = "Coffelalo234@gmail.com"
+// // email = "Razadeveloper123@gmail.com"
+
+// // let emailaddress = "dvhfeifbebfj@gmail.com"
+// // // number = 3436;
+// // // console.log(number);
+// // // console.log(email);
+// // let contantnumber;
+// // console.table([email,emailaddress,contantnumber])
+// // /*
+// // Not use of var
+// // not use var duee to block scope and functional scopee
+// // */
+// let name = "shahzad"
+// let raza = "suleman"
+// let raa = "suleman"
+
+// console.table([name,raza,raa])
+// var name = "shahzad"
+// var raza = "suleman"
+// var raa = "suleman"
+// console.table([name,raza,raa])
+// var name = "ywhd"
+// console.table([name])
+const name = "shahzad"
+const raza = "suleman"
+const raa = "suleman"
+console.table([name,raza,raa])
+const name = "ywhd"
+console.table([name])
